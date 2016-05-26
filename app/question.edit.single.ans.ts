e49e6587c7f39862f2df102c90c248ccac888bf4
Jacoub Bondre <jacoub.bondre@redlioncanada.com>
2016-05-26 14:42:54 -0400

    sideNav

--- app/question.edit.single.ans.ts
+++ app/question.edit.single.ans.ts
@@ -5,16 +5,45 @@ declare var Materialize;
 @Component({
     selector: 'question-single-answer-edit',
     template: `
-     <div class="container">
     <div class="row">
     <nav>
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo">Qualifier CMS - Single Answer Edit</a>
-    </div>
-  </nav>
+    <ul id="slide-out" class="side-nav">
+      <li><a href="#!">First Sidebar Link</a></li>
+      <li><a href="#!">Second Sidebar Link</a></li>
+      <li class="no-padding">
+        <ul class="collapsible collapsible-accordion">
+          <li>
+            <a class="collapsible-header">Dropdown<i class="mdi-navigation-arrow-drop-down"></i></a>
+            <div class="collapsible-body">
+              <ul>
+                <li><a href="#!">First</a></li>
+                <li><a href="#!">Second</a></li>
+                <li><a href="#!">Third</a></li>
+                <li><a href="#!">Fourth</a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <ul class="right hide-on-med-and-down">
+      <li><a href="#!">First Sidebar Link</a></li>
+      <li><a href="#!">Second Sidebar Link</a></li>
+      <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Dropdown<i class="mdi-navigation-arrow-drop-down right"></i></a></li>
+      <ul id='dropdown1' class='dropdown-content'>
+        <li><a href="#!">First</a></li>
+        <li><a href="#!">Second</a></li>
+        <li><a href="#!">Third</a></li>
+        <li><a href="#!">Fourth</a></li>
+      </ul>
+    </ul>
+    <a href="#" data-activates="slide-out" class="button-collapse"><i class="mdi-navigation-menu">hello</i></a>
+</nav>
   </div>
   <div class="row">
     <form class="col s12">
+    <p>What would you like to edit</p>
+    <h2>Question</h2>
       <div class="row">
         <div class="input-field col s6">
           <textarea id="question" class="materialize-textarea">For the Edit view, this will be pre-filled with the current question</textarea>
@@ -47,42 +76,31 @@ declare var Materialize;
   </div>
       </div>
       <div class="row">
+      <h2>Answers</h2>
   <ul class="sortable">
     <li id="line01">
-        <div class="card-panel teal">
-          <span class="white-text">I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
-          </span>
+        <div class="card-panel white">
+          <textarea id="answer_00" class="materialize-textarea">For the Edit view, this will be pre-filled with the current question</textarea>
         </div></li>
     <li>
-        <div class="card-panel teal">
-          <span class="white-text">I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
-          </span>
+        <div class="card-panel white">
+          <textarea id="answer_01" class="materialize-textarea">For the Edit view, this will be pre-filled with the current question</textarea>
         </div></li>
         <li>
-        <div class="card-panel teal">
-          <span class="white-text">I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
-          </span>
+        <div class="card-panel white">
+          <textarea id="answer_02" class="materialize-textarea">For the Edit view, this will be pre-filled with the current question</textarea>
         </div></li>
         <li>
-        <div class="card-panel teal">
-          <span class="white-text">I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
-          </span>
+        <div class="card-panel white">
+          <textarea id="answer_03" class="materialize-textarea">For the Edit view, this will be pre-filled with the current question</textarea>
         </div></li>
         <li>
-        <div class="card-panel teal">
-          <span class="white-text">I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
-          </span>
+        <div class="card-panel white">
+          <textarea id="answer_04" class="materialize-textarea">For the Edit view, this will be pre-filled with the current question</textarea>
         </div></li>
   </ul>
   </div>
     </form>
-  </div>
-  
   </div>
     `,
     directives: []
@@ -98,7 +116,13 @@ export class SingleAnswerEdit {
         $('#line01').mouseup(function(){
             setTimeout(function(){console.log($('#line01').index())},500)
         });
-        
+        $('.button-collapse').sideNav('show');
+        $('.button-collapse').sideNav({
+          menuWidth: 300, // Default is 240
+          edge: 'left', // Choose the horizontal origin
+          closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        }
+      );
     }
 	
 }
